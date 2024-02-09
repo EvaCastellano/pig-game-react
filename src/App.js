85a4544d3f2fd7e413656c9cc0ab4116cca495a7
EvaCastellano <evacastellano60@gmail.com>
2024-02-09 13:55:42 +0100

    Lógica de clases activa y winner

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <main>
       <Player name="Player 1" score={43} current={0} isActive={true} />
-      <Player name="Player 2" score={24} current={0} isActive={true} />
+      <Player name="Player 2" score={24} current={0} isActive={false} />
 
       <img src="dice-5.png" alt="Playing dice" className="dice" />
       <button className="btn btn--new">🔄 New game</button>
--- src/Player/Player.js
+++ src/Player/Player.js
@@ -1,9 +1,15 @@
 import './Player.css'
 // props = {name: 'Player 1' score:43, current:0, isActive: true}
-function Player({ name, score, current, isActive }) {
-  // =   const {n,s,etc} = props
+function Player({ name, score, current, isActive }) {// =   const {n,s,etc} = props
+
+  const classPlayer = isActive
+    ? score > 100
+      ? 'player player--winner'
+      : 'player player--active'
+    : 'player'
+
   return (
-    <section className="player player--active">
+    <section className={classPlayer}>
       <h2 className="name">{name}</h2>
       <p className="score">{score}</p>
       <div className="current">
