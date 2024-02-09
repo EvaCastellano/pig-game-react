19c95eb7199be0d8e7d6ff9d83777f741ac259f0
EvaCastellano <evacastellano60@gmail.com>
2024-02-09 13:25:18 +0100

    Adaptado el código a react

--- src/App.js
+++ src/App.js
@@ -1,36 +1,11 @@
 import './App.css'
+import Player from './Player/Player.js'
 
 function App() {
   return (
     <main>
-      <section className="player player--0 player--active">
-        <h2 className="name" id="name--0">
-          Player 1
-        </h2>
-        <p className="score" id="score--0">
-          43
-        </p>
-        <div className="current">
-          <p className="current-label">Current</p>
-          <p className="current-score" id="current--0">
-            0
-          </p>
-        </div>
-      </section>
-      <section className="player player--1">
-        <h2 className="name" id="name--1">
-          Player 2
-        </h2>
-        <p className="score" id="score--1">
-          24
-        </p>
-        <div className="current">
-          <p className="current-label">Current</p>
-          <p className="current-score" id="current--1">
-            0
-          </p>
-        </div>
-      </section>
+      <Player name="Player 1" score={43} current={0} isActive={true} />
+      <Player name="Player 2" score={24} current={0} isActive={true} />
 
       <img src="dice-5.png" alt="Playing dice" className="dice" />
       <button className="btn btn--new">🔄 New game</button>
